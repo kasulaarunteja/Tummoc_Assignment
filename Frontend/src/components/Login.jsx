72eb1a1cfb1ed361dd959cf1f17406191514d06f
kasulaarunteja <kasulaarunteja@gmail.com>
2022-09-22 18:28:34 +0530

    login

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,54 +1,49 @@
-
-
-import React from 'react';
-import "./login.css"
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./login.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div className="Auth-form-container">
-        <form className="Auth-form">
-          <div className="Auth-form-content">
-            <h3 className="Auth-form-title">Sign In</h3>
-            <div className="text-center">
-              Not registered yet?
-              <Link to="sing">
-                <span className="link-primary">Sign Up</span>
-              </Link>
-            </div>
-            <div className="form-group mt-3">
-              <label>Email address</label>
-              <br />
-              <input
-                type="email"
-                id="email"
-                className="form-control mt-1"
-                placeholder="Enter email"
-              />
-            </div>
-            <div className="form-group mt-3">
-              <label>Password</label>
-              <br />
-              <input
-                type="password"
-                id="password"
-                className="form-control mt-1"
-                placeholder="Enter password"
-              />
-            </div>
-            <br />
-            <div className="d-grid gap-2 mt-3">
-              <button
-                type="submit"
-                className="btn btn-primary"
-              >
-                Submit
-              </button>
-            </div>
+  return (
+    <div className="Auth-form-container">
+      <form className="Auth-form">
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign In</h3>
+          <div className="text-center">
+            Not registered yet?
+            <Link to="sing">
+              <span className="link-primary">Sign Up</span>
+            </Link>
           </div>
-        </form>
-      </div>
-    );
-}
+          <div className="form-group mt-3">
+            <label>Email address</label>
+            <br />
+            <input
+              type="email"
+              id="email"
+              className="form-control mt-1"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <br />
+            <input
+              type="password"
+              id="password"
+              className="form-control mt-1"
+              placeholder="Enter password"
+            />
+          </div>
+          <br />
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
--- Frontend/src/components/singin.jsx
+++ Frontend/src/components/singin.jsx
@@ -1,62 +1,59 @@
-
-
-import React from 'react';
-import "./login.css"
-import {Link, useNavigate} from "react-router-dom";
+import React from "react";
+import "./login.css";
+import { Link, useNavigate } from "react-router-dom";
 
 const Singin = () => {
-    return (
-        <div className="Auth-form-container">
-        <form className="Auth-form">
-          <div className="Auth-form-content">
-            <h3 className="Auth-form-title">Sign up</h3>
-            <div className="text-center">
-              Already registered?{" "}
-              <Link to="/">
-                <span className="link-primary">Sign In</span>
-              </Link>
-            </div>
-            <div className="form-group mt-3">
-              <label>Full Name</label>
-              <br />
-              <input
-                type="text"
-                id="name"
-                className="form-control mt-1"
-                placeholder="e.g Jane Doe"
-              />
-            </div>
-            <div className="form-group mt-3">
-              <label>Email address</label>
-              <br />
-              <input
-                type="email"
-                id="email"
-                className="form-control mt-1"
-                placeholder="Email Address"
-              />
-            </div>
-            <div className="form-group mt-3">
-              <label>Password</label>
-              <br />
-              <input
-                type="password"
-                id="password"
-                className="form-control mt-1"
-                placeholder="Password"
-              
-              />
-            </div>
-            <br />
-            <div className="d-grid gap-2 mt-3">
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-            </div>
+  return (
+    <div className="Auth-form-container">
+      <form className="Auth-form">
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign up</h3>
+          <div className="text-center">
+            Already registered?{" "}
+            <Link to="/">
+              <span className="link-primary">Sign In</span>
+            </Link>
           </div>
-        </form>
-      </div>
-    );
-}
+          <div className="form-group mt-3">
+            <label>Full Name</label>
+            <br />
+            <input
+              type="text"
+              id="name"
+              className="form-control mt-1"
+              placeholder="e.g Jane Doe"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Email address</label>
+            <br />
+            <input
+              type="email"
+              id="email"
+              className="form-control mt-1"
+              placeholder="Email Address"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <br />
+            <input
+              type="password"
+              id="password"
+              className="form-control mt-1"
+              placeholder="Password"
+            />
+          </div>
+          <br />
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Singin;
