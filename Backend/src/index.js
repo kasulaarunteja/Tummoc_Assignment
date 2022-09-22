a8848d392c05096b449b24fe2d1ab4a2b57a028e
kasulaarunteja <kasulaarunteja@gmail.com>
2022-09-22 12:44:12 +0530

    auth

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const connect = require('./configs/db');
-
-
 const cors = require('cors')
 
+
+const userController = require("./controllers/user.controller");
+const {register, login , newToken} = require("./controllers/auth.controller");
+
+
 const app = express()
-
 app.use(express.json())
-
 app.use(
   cors({
     origin: '*',
@@ -17,10 +18,14 @@ app.use(
 )
 
 
-
 app.get('/', (req, res) => {
   return res.send('hello good')
 })
+
+app.post("/register", register)
+app.post("/login", login)
+app.use("/users". userController)
+
 
 
 
--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -14,10 +14,18 @@ const userSchema = new mongoose.Schema(
   },
 )
 
-userSchema.pre('save', function(next) {
-    if(!this.isModified('password')) return next()
-    
-    var hash = bcrypt.hashSync(this.password, 8)
-    this.password = hash
-    
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password')) return next()
+
+  var hash = bcrypt.hashSync(this.password, 8)
+  this.password = hash
+  return next()
 })
+
+userSchema.method.checkPassword = function (password) {
+  return bcrypt.compareSync(password, this.password)
+}
+
+const User = mongoose.model('user', userSchema)
+
+module.exports = User
